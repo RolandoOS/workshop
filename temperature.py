550500c502ff06c946fb2aac8f9a2dcadd4addd8
RolandoOS <olivasrolando@gmail.com>
2015-06-26 10:37:15 -0400

    New temperature functions

--- temperature.py
+++ temperature.py
@@ -2,3 +2,11 @@ import numpy as np
 
 def f_to_k(temp):
     return ((temp - 32) * (5./9)) + 273.15
+
+def k_to_c(temp):
+    return temp-273.15
+
+def f_to_c(temp):
+    temp_k=f_to_k(temp)
+    temp=k_to_c(temp)
+    return temp
